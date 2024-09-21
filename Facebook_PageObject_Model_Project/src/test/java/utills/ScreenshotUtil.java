d29256d3d13f5795ae9f65664bbf301e56042d23
Md Hossain <mahossain3094@gmail.com>
2024-09-21 00:03:29 -0400

    Update my code

---
+++ Facebook_PageObject_Model_Project/src/test/java/utills/ScreenshotUtil.java
@@ -0,0 +1,27 @@
+package utills;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+public class ScreenshotUtil {
+    private WebDriver driver;
+
+    public ScreenshotUtil(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void takeScreenshot(String filePath) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(source, new File(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Rest_Assured_Api_Maven_Project/src/test/java/apitest/Rest_Api_Test.java
@@ -0,0 +1,76 @@
+package apitest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+public class Rest_Api_Test {
+
+    int id;
+
+    @Test(priority = 1)
+    void getUsers() {
+        given()
+
+        .when()
+            .get("https://reqres.in/api/users?page=2")
+        .then()
+            .statusCode(200)
+            .body("page", equalTo(2))
+            .log().all();
+    }
+
+    @Test(priority = 2)
+    void createUser() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("name", "Md ");
+        data.put("job", "TA");
+
+        // Send request and validate response
+        id = given()
+            .contentType("application/json")   
+            .body(data)
+
+        .when()
+            .post("https://reqres.in/api/users")
+
+        .then()
+            .statusCode(201)
+            .log().all()  // Log the response
+            .extract()    // Extract response
+            .jsonPath().getInt("id");  // Extract 'id' from the response
+    }
+
+    @Test(priority = 3, dependsOnMethods = {"createUser"})
+    void updateUser() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("name", "Ayan ");
+        data.put("job", "Student");
+
+        given()
+            .contentType("application/json")   
+            .body(data)
+
+        .when()
+            .put("https://reqres.in/api/users/" + id)
+
+        .then()
+            .statusCode(200)
+            .log().all();                     
+    }
+
+    @Test(priority = 4)
+    void deleteUser() {
+        given()
+
+        .when()
+            .delete("https://reqres.in/api/users/" + id)
+
+        .then()
+            .statusCode(204)
+            .log().all();
+    }
+}
